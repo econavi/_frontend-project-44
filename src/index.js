a78b565b23c218594fc6a7aefee382d71effaa82
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-01-15 18:08:24 +0300

    Fix export index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import readlineSync from 'readline-sync';
 
 const userName = () => readlineSync.question('May I have your name? ');
 
-export const sayHello = () => {
+const sayHello = () => {
   console.log('Welcome to the Brain Games!');
   console.log(`Hello, ${userName()}!`);
 };
+
+export default sayHello;
