507274c6b1b505b6e42a69ae1045946ed08b3d75
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-01-16 12:29:25 +0300

    Refactoring the Even Game

--- src/even.js
+++ src/even.js
@@ -2,11 +2,8 @@ import readlineSync from 'readline-sync';
 
 const roundsLimit = 3;
 const isEven = num => num % 2 === 0;
-const insertDelimiter = () => console.log('\n');
 
-const startGame = (userName) => {
-  let userIsWinner = 'false';
-
+const startGame = () => {
   const iterRound = (limit) => {
     if (limit < 1) return;
 
@@ -16,30 +13,30 @@ const startGame = (userName) => {
     const correctAnswer = isEven(question) ? 'yes' : 'no';
 
     if (answer !== correctAnswer) {
-      userIsWinner = false;
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
+      return false; // eslint-disable-line consistent-return
     }
 
     console.log('Correct!');
-    userIsWinner = true;
     iterRound(limit - 1);
   };
 
   iterRound(roundsLimit);
-  if (userIsWinner) console.log(`Congratulations, ${userName}!`);
+  return true;
 };
 
 export default () => {
-  insertDelimiter();
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".');
-  insertDelimiter();
-
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  insertDelimiter();
 
-  startGame(userName);
+  const userIsWinner = startGame();
+
+  if (!userIsWinner) {
+    console.log(`Let's try again, ${userName}!`);
+    return;
+  }
+
+  console.log(`Congratulations, ${userName}!`);
 };
