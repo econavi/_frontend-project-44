a88df67c7c919e62b6e65b109a8a5471578400e2
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-01-20 12:50:03 +0300

    Refactoring isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,12 +4,13 @@ import runGame from '..';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num < 2) return false;
+  if (num <= 1) return false;
+  if (num === 2) return true;
 
-  for (let i = num - 1; i >= 2; i -= 1) {
-    if (num % i === 0) {
-      return false;
-    }
+  const sqrt = Math.sqrt(num);
+
+  for (let i = 2; i <= sqrt; i += 1) {
+    if (num % i === 0) return false;
   }
 
   return true;
